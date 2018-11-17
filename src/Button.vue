2cc01a96cfa18fb73710d3d4b340dbad6edcf651
no1harm <chen09869@gmail.com>
2018-11-17 15:30:30 +0800

    引入iconfont

--- src/Button.vue
+++ src/Button.vue
@@ -1,31 +1,34 @@
 <template>
     <button class="hy-button">
+        <svg class="icon" v-if="icon"aria-hidden="true">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
         <slot></slot>
     </button>
 </template>
 
 <script>
 export default {
-    
-}
+    props:['icon']
+};
 </script>
 
 <style lang="scss">
-.hy-button{
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    &:hover{
-        border: 1px solid var(--border-color-hover);
-    }
-    &:active{
-        background: var(--button-active-bg);
-    }
-    &:focus{
-        outline: none;
-    }
+.hy-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border: 1px solid var(--border-color-hover);
+  }
+  &:active {
+    background: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
 }
 </style>
