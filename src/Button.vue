904127a2f8f89754298e21705cac1df17bf96fc0
no1harm <chen09869@gmail.com>
2018-11-17 16:07:03 +0800

    调整icon的位置

--- src/Button.vue
+++ src/Button.vue
@@ -1,34 +1,40 @@
 <template>
-    <button class="hy-button">
-        <svg class="icon" v-if="icon"aria-hidden="true">
-            <use :xlink:href=`#i-${icon}`></use>
+    <button class="hy-button" :class="{[`icon-${icontPostition}`]:true}">
+        <svg class="icon" v-if="icon" aria-hidden="true" >
+            <use :xlink:href=`#i-${icon}` ></use>
         </svg>
-        <slot></slot>
+        <div class="content">
+            <slot></slot>
+        </div>
     </button>
 </template>
 
 <script>
 export default {
-    props:['icon']
+    props:['icon','icontPostition'],
 };
 </script>
 
 <style lang="scss">
 .hy-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   font-size: var(--font-size);
   height: var(--button-height);
   padding: 0 1em;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
-  &:hover {
-    border: 1px solid var(--border-color-hover);
-  }
-  &:active {
-    background: var(--button-active-bg);
-  }
-  &:focus {
-    outline: none;
+  &:hover {border: 1px solid var(--border-color-hover);}
+  &:active {background: var(--button-active-bg);}
+  &:focus {outline: none;}
+  > .icon{order: 1;margin-right: .3em;}
+  > .content{order: 2}
+  &.icon-right{
+      .content{order: 1}
+      .icon{order: 2;margin-left: .3em;margin-right: 0;}
   }
 }
 </style>
