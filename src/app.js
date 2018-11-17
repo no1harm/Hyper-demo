d9682dad54750093bce2e7056e063f74ccfa1ba3
no1harm <chen09869@gmail.com>
2018-11-17 19:40:23 +0800

    创建 button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './Button.vue'
 import Icon from './Icon.vue'
+import ButtonGroup from './buttonGroup.vue'
 
 Vue.component('hy-button',Button)
 Vue.component('hy-icon',Icon)
+Vue.component('hy-button-group',ButtonGroup)
 
 new Vue({
     el:"#app",
