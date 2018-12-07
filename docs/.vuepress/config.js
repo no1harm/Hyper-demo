cbab549c9e46e3ed89c42cb8c60588a3e05fdf98
no1harm <chen09869@gmail.com>
2018-12-07 15:50:55 +0800

    使用vuepress介绍组件库

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  title: 'Hyper UI',
+  description: '一个基于 Vue 的 UI 组件库',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '文档', link: '/' },
+      { text: '交流', link: '/' }
+    ],
+    sidebar: [
+      '/',
+      {
+        title: '入门',
+        children: [
+          '/install/',
+          '/get-start/',
+        ]
+      },
+      {
+        title: '组件',
+        children: [
+          '/components/button.md'
+        ]
+      }
+    ]
+  }
+}
